f8d36e78c52cabfb805ba00a49f96cc1dcdae6df
jtdunlop <jeff@thedunlops.id.au>
2014-11-26 13:31:41 +1100

    Add CacheProvider

---
+++ Cache.Aspect/CacheProvider.cs
@@ -0,0 +1,90 @@
+﻿namespace DbSoft.Cache.Aspect
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.Caching;
+
+    // Key structure:
+    // guid(session).keyname.serialization
+    // if no session and no keyname, ..serialization
+    // Delete by session: starts with session + '.';
+    // Delete by key: contains '.' + keyname + '.';
+
+    public class CacheProvider : ICache
+    {
+        public static CacheProvider Cache;
+
+        public CacheProvider()
+        {
+            _cache = MemoryCache.Default;
+            _keys = new HashSet<string>();
+        }
+
+        public bool Contains(string key)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // return false;
+            }
+            return _cache[key] != null;
+        }
+
+        public void Delete(string key)
+        {
+            _cache.Remove(key);
+            if (_keys.Contains(key))
+            {
+                _keys.Remove(key);
+            }
+        }
+
+        public void DeleteAll(string @session)
+        {
+            ClearAll(session);
+        }
+
+        public void DeleteAll()
+        {
+            ClearAll(null);
+        }
+
+        public object this[string key]
+        {
+            get
+            {
+                return _cache[key];
+            }
+            set
+            {
+                _cache[key] = value;
+                if (!_keys.Contains(key))
+                {
+                    _keys.Add(key);
+                }
+            }
+        }
+
+        private void ClearAll(string searchKey)
+        {
+            if (string.IsNullOrEmpty(searchKey))
+            {
+                foreach (var key in _keys)
+                {
+                    _cache.Remove(key);
+                }
+                _keys.Clear();
+            }
+            else
+            {
+                var keys = _keys.Where(k => k.Contains(searchKey)).ToList();
+                foreach (var key in keys)
+                {
+                    _keys.Remove(key);
+                    _cache.Remove(key);
+                }
+            }
+        }
+        private readonly ObjectCache _cache;
+        private readonly HashSet<string> _keys;
+    }
+}
